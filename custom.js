ac946805337a965cc1c080d42e07d989f988c35d
Tuan Bach Van <bachvtuan@gmail.com>
2014-05-14 22:10:44 +0700

    remove unuse files.

---
+++ custom.js
@@ -0,0 +1,18 @@
+
+      window.onload = function () {
+        document.getElementById("button").onclick = function () {
+          try {
+            var result = jsonlint.parse(document.getElementById("source").value);
+            if (result) {
+              document.getElementById("result").innerHTML = "JSON is valid!";
+              document.getElementById("result").className = "pass";
+              if (document.getElementById("reformat").checked) {
+                document.getElementById("source").value = JSON.stringify(result, null, "  ");
+              }
+            }
+          } catch(e) {
+            document.getElementById("result").innerHTML = e;
+            document.getElementById("result").className = "fail";
+          }
+        };
+      }
